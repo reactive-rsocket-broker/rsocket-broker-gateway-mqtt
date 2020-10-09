1ef77164ee76fb76daae9d6f999d7a3c05a2d7d0
linux_china <libing.chen@gmail.com>
2020-10-08 23:26:47 -0700

    Spring Boot base test case

---
+++ src/test/java/com/alibaba/rsocket/mqtt/gateway/SpringBootBaseTestCase.java
@@ -0,0 +1,12 @@
+package com.alibaba.rsocket.mqtt.gateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * spring boot base test case
+ *
+ * @author leijuan
+ */
+@SpringBootTest
+public abstract class SpringBootBaseTestCase {
+}
